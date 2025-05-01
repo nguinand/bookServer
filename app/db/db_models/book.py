dfba9c97209fa88354f2a50f40c7fe58fd799a00
nmguinand <nmguinand@hotmail.com>
2025-04-30 23:02:24 -0400

    added relationships to book.py

--- app/db/db_models/book.py
+++ app/db/db_models/book.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship
-from base import Base
-from book_authors import book_authors
+from app.db.db_models.base import Base
 
 
 class Book(Base):
@@ -24,8 +23,13 @@ class Book(Base):
 
     publisher = relationship("Publisher", back_populates="books")
     # A book can have multiple authors. N:N
-    authors = relationship("Author", secondary=book_authors, back_populates="books")
+    authors = relationship("Author", secondary="book_authors", back_populates="books")
     sale_info = relationship("BookSale", back_populates="book", uselist=False)
     access_info = relationship("BookAccess", back_populates="book", uselist=False)
     identifiers = relationship("BookIdentifier", back_populates="book")
     genres = relationship("Genre", secondary="book_genres", back_populates="books")
+    ratings = relationship("UserBookAttributes", back_populates="book")
+
+    bookcases = relationship(
+        "Bookcase", secondary="bookcase_books", back_populates="books"
+    )
