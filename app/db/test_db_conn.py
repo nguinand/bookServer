eec73a3f65bdf80f825a75479c51ad86ecad84c9
nmguinand <nmguinand@hotmail.com>
2025-05-05 23:01:45 -0400

    formatted env and logger files

--- app/db/test_db_conn.py
+++ app/db/test_db_conn.py
@@ -1,6 +1,7 @@
 from app.db.db_conn import db_manager
 from app.db.db_models.user import User
 print(User)
+print("ASE")
 from sqlalchemy.inspection import inspect
 
 print("Mapped columns:", inspect(User).columns.keys())
@@ -9,4 +10,5 @@ def test_conn():
     print(type(User))
     with db_manager.session as session:
         users = session.query(User).all()
+        print(users)
         assert True
