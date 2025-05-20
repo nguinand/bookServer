e40bd088f86bbd6138d2df2ed3f7f960cf3a6a7f
nmguinand <nmguinand@hotmail.com>
2025-05-19 23:41:58 -0400

    created crud commands for author

--- app/crud/author_crud.py
+++ app/crud/author_crud.py
@@ -1,8 +1,10 @@
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
+from app.crud.book_crud import BookModel
 from app.db.db_models.author import Author
+from app.db.db_models.book import Book
 
 
 class AuthorModel(BaseModel):
@@ -10,6 +12,7 @@ class AuthorModel(BaseModel):
     google_books_id: Optional[str] = None
     bio: Optional[str] = None
     name: str
+    books: List[BookModel] = []
 
     class Config:
         from_attributes = True
@@ -26,12 +29,49 @@ class AuthorCrud:
         session.refresh(author_data)
         return author_data
 
-    def get_author_by_name(self, name: str, session:Session) -> list[Author]:
-        return session.query(Author).filter_by(name=name).all()
-    
-    def get_author_by_id(self, id: int, session: Session) -> Author:
-        return session.query(Author).filter_by(id = id).first()
+    def get_author_by_name(self, name: str, session: Session) -> list[AuthorModel]:
+        authors = session.query(Author).filter_by(name=name).all()
+        return [AuthorModel.model_validate(x) for x in authors]
 
-    def update_author(self): ...
+    def get_author_by_id(self, id: int, session: Session) -> AuthorModel:
+        author_record = session.query(Author).filter_by(id=id).first()
+        return AuthorModel.model_validate(author_record)
 
-    def delete_author(self): ...
+    def update_author(
+        self, author_replacement: AuthorModel, session: Session
+    ) -> None | AuthorModel:
+        # find original author and replace contents
+        if author_replacement.id is None:
+            raise ValueError(
+                f"Cannot replace author without an ID. {author_replacement.id} - {author_replacement.name}"
+            )
+
+        author_record = (
+            session.query(Author).filter_by(id=author_replacement.id).first()
+        )
+
+        if not author_record:
+            return None
+
+        author_record.name = author_replacement.name
+        author_record.bio = author_replacement.bio
+        author_record.google_books_id = author_replacement.google_books_id
+        author_record.books = [
+            session.query(Book).filter_by(id=book.id).first()
+            for book in author_replacement.books
+            if book.id is not None
+        ]
+        author_record.books = author_replacement.books
+        session.commit()
+
+        return author_record
+
+    def delete_author_by_id(self, author_id: int, session: Session) -> bool:
+        author = session.query(Author).filter_by(id=author_id).first()
+
+        if not author:
+            return False  # could have returned an exception
+
+        session.delete(author)
+        session.commit()
+        return True
