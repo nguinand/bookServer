8d8c88973a2954589a8359acf93edc99d57038ef
nmguinand <nmguinand@hotmail.com>
2025-05-22 19:08:00 -0400

    created crud operations for book sale info

---
+++ app/crud/book_sale_info_crud.py
@@ -0,0 +1,113 @@
+from app.db.db_models.book_sale_info import BookSaleInfo
+from app.models.book_sale_info import BookSaleInfoModel, CurrencyCode, PriceModel
+from sqlalchemy.orm import Session
+
+
+class BookSaleInfoCrud:
+    def create_book_sale_info(
+        self, book_sale_info_model: BookSaleInfoModel, session: Session
+    ) -> BookSaleInfoModel:
+        book_sale_info_data = BookSaleInfo(
+            book_id=book_sale_info_model.book_id,
+            country=book_sale_info_model.country,
+            saleability=book_sale_info_model.saleability,
+            is_ebook=book_sale_info_model.is_ebook,
+        )
+
+        # list_price and retail_price are both related to the PriceModel pydantic model. We need to invidually extract their values.
+
+        if book_sale_info_model.list_price is not None:
+            book_sale_info_data.list_price = book_sale_info_model.list_price.amount
+            book_sale_info_data.list_price_currency_code = book_sale_info_model.list_price.currencyCode
+
+        if book_sale_info_model.retail_price is not None:
+            book_sale_info_data.retail_price = book_sale_info_model.retail_price.amount
+            book_sale_info_data.retail_price_currency_code =book_sale_info_model.retail_price.currencyCode
+
+        session.add(book_sale_info_data)
+        session.commit()
+        session.refresh(book_sale_info_data)
+
+        # Convert the db model to a pydantic model. To return that.
+        return self.convert_book_sale_info(book_sale_info_data)
+
+    def get_book_sale_info_by_id(
+        self, id: int, session: Session
+    ) -> BookSaleInfoModel | None: 
+        book_sale_info_record = session.query(BookSaleInfo).filter_by(id=id).first()
+        try:
+            return self.convert_book_sale_info(book_sale_info_record)
+        except AttributeError as e:
+            print(e)
+            return None
+
+    def update_book_sale_info(
+        self, book_sale_info_replacement: BookSaleInfoModel, session: Session
+    ) -> None | BookSaleInfoModel:
+        if book_sale_info_replacement.id is None:
+            raise ValueError(
+                f"Cannot replace a book sale info record without an ID. {book_sale_info_replacement.id}"
+            )
+        
+        book_sale_info_record = (
+            session.query(BookSaleInfo).filter_by(id=book_sale_info_replacement.id).first()
+        )
+
+        if not book_sale_info_record:
+            return None
+        
+        book_sale_info_record.id = book_sale_info_replacement.id
+        book_sale_info_record.book_id = book_sale_info_replacement.book_id
+        book_sale_info_record.country = book_sale_info_replacement.country
+        book_sale_info_record.saleability = book_sale_info_replacement.saleability
+        book_sale_info_record.is_ebook = book_sale_info_replacement.is_ebook
+        book_sale_info_record.buy_link = book_sale_info_replacement.buy_link
+
+        if book_sale_info_replacement.list_price is not None:
+            book_sale_info_record.list_price = book_sale_info_replacement.list_price.amount
+            book_sale_info_record.list_price_currency_code = book_sale_info_replacement.list_price.currencyCode
+
+        if book_sale_info_replacement.retail_price is not None:
+            book_sale_info_record.retail_price = book_sale_info_replacement.retail_price.amount
+            book_sale_info_record.retail_price_currency_code =book_sale_info_replacement.retail_price.currencyCode
+
+        session.commit()
+        return self.convert_book_sale_info(book_sale_info_record)
+    
+    def delete_book_sale_info(
+        self, book_sale_info_id: int, session: Session
+    ) -> bool: 
+        book_sale_info = session.query(BookSaleInfo).filter_by(id=book_sale_info_id).first()
+
+        if not book_sale_info:
+            return False
+        
+        session.delete(book_sale_info)
+        session.commit()
+        return True
+
+    def convert_book_sale_info(self,book_sale_info_data: BookSaleInfo) -> BookSaleInfoModel:
+        converted_book_sale_info = BookSaleInfoModel(
+            id=book_sale_info_data.id,
+            book_id=book_sale_info_data.book_id,
+            buyLink=book_sale_info_data.buy_link,
+            country=book_sale_info_data.country,
+            isEbook=book_sale_info_data.is_ebook,
+            saleability=book_sale_info_data.saleability,
+            listPrice=PriceModel(
+                amount=book_sale_info_data.list_price,
+                currencyCode=CurrencyCode(book_sale_info_data.list_price_currency_code)
+                if book_sale_info_data.list_price_currency_code
+                else None,
+            ),
+            retailPrice=PriceModel(
+                amount=book_sale_info_data.retail_price,
+                currencyCode=CurrencyCode(
+                    book_sale_info_data.retail_price_currency_code
+                )
+                if book_sale_info_data.retail_price_currency_code
+                else None,
+            ),
+        )
+
+        return converted_book_sale_info
---
+++ app/db/db_models/book_sale_info.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from sqlalchemy import DECIMAL, Boolean, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from app.db.db_models.base import Base
+from decimal import Decimal
+
+
+class BookSaleInfo(Base):
+    __tablename__ = "book_sale_info"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True, autoincrement=True
+    )
+    book_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("books.id"), nullable=False
+    )
+    country: Mapped[Optional[str]] = mapped_column(String(10), nullable=True)
+    saleability: Mapped[Optional[str]] = mapped_column(String(50), nullable=True)
+    is_ebook: Mapped[Optional[bool]] = mapped_column(Boolean, nullable=True)
+    buy_link: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    list_price: Mapped[Optional[Decimal]] = mapped_column(DECIMAL(10, 2), nullable=True)
+    list_price_currency_code: Mapped[Optional[str]] = mapped_column(String(5), nullable=True)
+    retail_price: Mapped[Optional[Decimal]] = mapped_column(
+        DECIMAL(10, 2), nullable=True
+    )
+    retail_price_currency_code: Mapped[Optional[str]] = mapped_column(
+        String(5), nullable=True
+    )
+
+    book = relationship("Book", back_populates="book_sale_info")
