e1068bc0c94f82d87b0294c2f714ef724a9ac9ee
nmguinand <nmguinand@hotmail.com>
2025-05-31 20:16:34 -0400

    cascade delete for user book attributes

--- app/models/user_book_attributes.py
+++ app/models/user_book_attributes.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
-
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 # This is the USER input for the book.
 class UserBookAttributesModel(BaseModel):
     id: int
     user_id: int
-    book_id: int
+    book_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("books.id", ondelete="CASCADE"), nullable=False
+    )
     rating: int
     review_text: Optional[str] = ""
     created_at: Optional[datetime] = None
