3a33a8de6bd3b2222a6d8488452ee37a8e08eea3
nmguinand <nmguinand@hotmail.com>
2025-05-31 20:15:33 -0400

    added cascade deletion for book relationships

--- app/db/db_models/book.py
+++ app/db/db_models/book.py
@@ -29,13 +29,20 @@ class Book(Base):
 
     # A book can have multiple authors. N:N
     authors = relationship("Author", secondary="book_authors", back_populates="books")
-    book_sale_info = relationship("BookSaleInfo", back_populates="book", uselist=False)
+    book_sale_info = relationship(
+        "BookSaleInfo",
+        back_populates="book",
+        uselist=False,
+        cascade="all, delete-orphan",
+    )
     access_info = relationship("BookAccess", back_populates="book", uselist=False)
     identifiers = relationship(
         "BookIdentifier", back_populates="book", cascade="all, delete-orphan"
     )
     genres = relationship("Genre", secondary="book_genres", back_populates="books")
-    ratings = relationship("UserBookAttributes", back_populates="book")
+    ratings = relationship(
+        "UserBookAttributes", back_populates="book", cascade="all, delete-orphan"
+    )
 
     bookcases = relationship(
         "Bookcase", secondary="bookcase_books", back_populates="books"
