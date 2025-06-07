1d00637f8282e8d59916a62942551f7612badf41
nmguinand <nmguinand@hotmail.com>
2025-06-07 14:00:40 -0400

    created genre crud and removed class from crud files

--- app/crud/book_sale_info_crud.py
+++ app/crud/book_sale_info_crud.py
@@ -1,120 +1,114 @@
 from app.db.db_models.book_sale_info import BookSaleInfo
-from app.db.sql_queries.book_sale_info_queries import get_book_sale_info_by_id
 from app.models.book_sale_info import BookSaleInfoModel, CurrencyCode, PriceModel
 from sqlalchemy.orm import Session
 
 
-class BookSaleInfoCrud:
-    def create_book_sale_info(
-        self, book_sale_info_model: BookSaleInfoModel, session: Session
-    ) -> BookSaleInfo:
-        book_sale_info_data = BookSaleInfo(
-            book_id=book_sale_info_model.book_id,
-            country=book_sale_info_model.country,
-            saleability=book_sale_info_model.saleability,
-            is_ebook=book_sale_info_model.is_ebook,
+def create_book_sale_info(
+    book_sale_info_model: BookSaleInfoModel, session: Session
+) -> BookSaleInfo:
+    book_sale_info_data = BookSaleInfo(
+        book_id=book_sale_info_model.book_id,
+        country=book_sale_info_model.country,
+        saleability=book_sale_info_model.saleability,
+        is_ebook=book_sale_info_model.is_ebook,
+    )
+
+    # list_price and retail_price are both related to the PriceModel pydantic model. We need to invidually extract their values.
+
+    if book_sale_info_model.list_price is not None:
+        book_sale_info_data.list_price = book_sale_info_model.list_price.amount
+        book_sale_info_data.list_price_currency_code = (
+            book_sale_info_model.list_price.currencyCode
         )
 
-        # list_price and retail_price are both related to the PriceModel pydantic model. We need to invidually extract their values.
-
-        if book_sale_info_model.list_price is not None:
-            book_sale_info_data.list_price = book_sale_info_model.list_price.amount
-            book_sale_info_data.list_price_currency_code = (
-                book_sale_info_model.list_price.currencyCode
-            )
-
-        if book_sale_info_model.retail_price is not None:
-            book_sale_info_data.retail_price = book_sale_info_model.retail_price.amount
-            book_sale_info_data.retail_price_currency_code = (
-                book_sale_info_model.retail_price.currencyCode
-            )
-
-        session.add(book_sale_info_data)
-        session.commit()
-        session.refresh(book_sale_info_data)
-
-        return book_sale_info_data
-
-    def get_book_sale_info_by_id(
-        self, id: int, session: Session
-    ) -> BookSaleInfo | None:
-        return get_book_sale_info_by_id(id, session)
-
-    def update_book_sale_info(
-        self, book_sale_info_replacement: BookSaleInfoModel, session: Session
-    ) -> None | BookSaleInfo:
-        if book_sale_info_replacement.id is None:
-            raise ValueError(
-                f"Cannot replace a book sale info record without an ID. {book_sale_info_replacement.id}"
-            )
-
-        book_sale_info_record = get_book_sale_info_by_id(
-            book_sale_info_replacement.id, session
+    if book_sale_info_model.retail_price is not None:
+        book_sale_info_data.retail_price = book_sale_info_model.retail_price.amount
+        book_sale_info_data.retail_price_currency_code = (
+            book_sale_info_model.retail_price.currencyCode
         )
 
-        if not book_sale_info_record:
-            return None
+    session.add(book_sale_info_data)
+    session.commit()
+    session.refresh(book_sale_info_data)
 
-        book_sale_info_record.id = book_sale_info_replacement.id
-        book_sale_info_record.book_id = book_sale_info_replacement.book_id
-        book_sale_info_record.country = book_sale_info_replacement.country
-        book_sale_info_record.saleability = book_sale_info_replacement.saleability
-        book_sale_info_record.is_ebook = book_sale_info_replacement.is_ebook
-        book_sale_info_record.buy_link = book_sale_info_replacement.buy_link
+    return book_sale_info_data
 
-        if book_sale_info_replacement.list_price is not None:
-            book_sale_info_record.list_price = (
-                book_sale_info_replacement.list_price.amount
-            )
-            book_sale_info_record.list_price_currency_code = (
-                book_sale_info_replacement.list_price.currencyCode
-            )
 
-        if book_sale_info_replacement.retail_price is not None:
-            book_sale_info_record.retail_price = (
-                book_sale_info_replacement.retail_price.amount
-            )
-            book_sale_info_record.retail_price_currency_code = (
-                book_sale_info_replacement.retail_price.currencyCode
-            )
+def get_book_sale_info_by_id(id: int, session: Session) -> BookSaleInfo | None:
+    return session.query(BookSaleInfo).filter_by(id=id).first()
 
-        session.commit()
-        return book_sale_info_record
 
-    def delete_book_sale_info(self, book_sale_info_id: int, session: Session) -> bool:
-        book_sale_info = get_book_sale_info_by_id(book_sale_info_id, session)
-
-        if not book_sale_info:
-            return False
-
-        session.delete(book_sale_info)
-        session.commit()
-        return True
-
-    def convert_book_sale_info(
-        self, book_sale_info_data: BookSaleInfo
-    ) -> BookSaleInfoModel:
-        converted_book_sale_info = BookSaleInfoModel(
-            id=book_sale_info_data.id,
-            book_id=book_sale_info_data.book_id,
-            buyLink=book_sale_info_data.buy_link,
-            country=book_sale_info_data.country,
-            isEbook=book_sale_info_data.is_ebook,
-            saleability=book_sale_info_data.saleability,
-            listPrice=PriceModel(
-                amount=book_sale_info_data.list_price,
-                currencyCode=CurrencyCode(book_sale_info_data.list_price_currency_code)
-                if book_sale_info_data.list_price_currency_code
-                else None,
-            ),
-            retailPrice=PriceModel(
-                amount=book_sale_info_data.retail_price,
-                currencyCode=CurrencyCode(
-                    book_sale_info_data.retail_price_currency_code
-                )
-                if book_sale_info_data.retail_price_currency_code
-                else None,
-            ),
+def update_book_sale_info(
+    book_sale_info_replacement: BookSaleInfoModel, session: Session
+) -> None | BookSaleInfo:
+    if book_sale_info_replacement.id is None:
+        raise ValueError(
+            f"Cannot replace a book sale info record without an ID. {book_sale_info_replacement.id}"
         )
 
-        return converted_book_sale_info
+    book_sale_info_record = get_book_sale_info_by_id(
+        book_sale_info_replacement.id, session
+    )
+
+    if not book_sale_info_record:
+        return None
+
+    book_sale_info_record.id = book_sale_info_replacement.id
+    book_sale_info_record.book_id = book_sale_info_replacement.book_id
+    book_sale_info_record.country = book_sale_info_replacement.country
+    book_sale_info_record.saleability = book_sale_info_replacement.saleability
+    book_sale_info_record.is_ebook = book_sale_info_replacement.is_ebook
+    book_sale_info_record.buy_link = book_sale_info_replacement.buy_link
+
+    if book_sale_info_replacement.list_price is not None:
+        book_sale_info_record.list_price = book_sale_info_replacement.list_price.amount
+        book_sale_info_record.list_price_currency_code = (
+            book_sale_info_replacement.list_price.currencyCode
+        )
+
+    if book_sale_info_replacement.retail_price is not None:
+        book_sale_info_record.retail_price = (
+            book_sale_info_replacement.retail_price.amount
+        )
+        book_sale_info_record.retail_price_currency_code = (
+            book_sale_info_replacement.retail_price.currencyCode
+        )
+
+    session.commit()
+    return book_sale_info_record
+
+
+def delete_book_sale_info(book_sale_info_id: int, session: Session) -> bool:
+    book_sale_info = get_book_sale_info_by_id(book_sale_info_id, session)
+
+    if not book_sale_info:
+        return False
+
+    session.delete(book_sale_info)
+    session.commit()
+    return True
+
+
+def convert_book_sale_info(book_sale_info_data: BookSaleInfo) -> BookSaleInfoModel:
+    converted_book_sale_info = BookSaleInfoModel(
+        id=book_sale_info_data.id,
+        book_id=book_sale_info_data.book_id,
+        buyLink=book_sale_info_data.buy_link,
+        country=book_sale_info_data.country,
+        isEbook=book_sale_info_data.is_ebook,
+        saleability=book_sale_info_data.saleability,
+        listPrice=PriceModel(
+            amount=book_sale_info_data.list_price,
+            currencyCode=CurrencyCode(book_sale_info_data.list_price_currency_code)
+            if book_sale_info_data.list_price_currency_code
+            else None,
+        ),
+        retailPrice=PriceModel(
+            amount=book_sale_info_data.retail_price,
+            currencyCode=CurrencyCode(book_sale_info_data.retail_price_currency_code)
+            if book_sale_info_data.retail_price_currency_code
+            else None,
+        ),
+    )
+
+    return converted_book_sale_info
