0658ad185f282178620d4a7a69d48b7c8b819ceb
nmguinand <nmguinand@hotmail.com>
2025-06-07 14:01:05 -0400

    removed sql query files

--- app/db/sql_queries/avatar_queries.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Session
-
-from app.db.db_models.avatar import Avatar
-
-
-def get_avatar_by_id(id: int, session: Session) -> Avatar:
-    return session.query(Avatar).filter_by(id=id).first()
--- app/db/sql_queries/user_status_queries.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Session
-
-from app.db.db_models.user_status import UserStatus
-
-
-def get_user_status_by_id(id: int, session: Session) -> UserStatus:
-    return session.query(UserStatus).filter_by(id=id).first()
