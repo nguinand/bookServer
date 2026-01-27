4331503569f8df71ce8a31783895184f6c15b2ee
nmguinand <nmguinand@hotmail.com>
2026-01-26 22:34:20 -0500

    router now has books by isbn

--- app/api/books/router.py
+++ app/api/books/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .books_by_name import router as books_by_name
+from .books_by_isbn import router as books_by_isbn
 
 router = APIRouter(prefix="/books", tags=["books"])
 
 router.include_router(books_by_name)
+router.include_router(books_by_isbn)
