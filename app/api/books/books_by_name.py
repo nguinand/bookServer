8ca305eddec9aba2a29daed4b9faf662ee69261a
nmguinand <nmguinand@hotmail.com>
2026-01-26 22:34:01 -0500

    books by name now uses helper httpx function

--- app/api/books/books_by_name.py
+++ app/api/books/books_by_name.py
@@ -2,48 +2,34 @@ from typing import List
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from app.utils.get_env import get_env_val_or_raise
+
+from app import api_key, book_api_request, BooksRequestError
 from app.utils.logger import get_logger
 import httpx
 
 from app.models.book import BookModel
 
-logger = get_logger()
+logger = get_logger(__name__)
 
 
 router = APIRouter()
-GOOGLE_BOOKS_API_URL = get_env_val_or_raise("GOOGLE_BOOKS_API_URL")
-api_key = get_env_val_or_raise("GOOGLE_BOOKS_API_KEY")
 
 
-@router.get("/name/", response_model=List[BookModel])
+@router.get("/name/", response_model=List[BookModel], status_code=200)
 async def get_books_by_name(
-    query: str, max_results: int = 10, start_index: int = 0
+    book_name: str, max_results: int = 10, start_index: int = 0
 ) -> List[BookModel]:
     params = {
-        "q": query,
+        "q": book_name,
         "startIndex": start_index,
         "maxResults": max_results,
         "key": api_key,
     }
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(GOOGLE_BOOKS_API_URL, params=params)
-            response.raise_for_status()
-        except httpx.HTTPStatusError as e:
-            logger.error(e.response.text)
-            return JSONResponse(
-                status_code=e.response.status_code, content=e.response.json()
-            )
-        except httpx.RequestError as e:
-            logger.error(f"Network error calling Google Books API, {str(e)}")
-            return JSONResponse(
-                status_code=502,
-                content={
-                    "error": "Upstream request to Google Books failed",
-                    "detail": str(e),
-                },
-            )
+            response = await book_api_request(client, params, logger)
+        except BooksRequestError as e:
+            return JSONResponse(status_code=e.status_code, content=e.error)
 
         data = response.json()
 
