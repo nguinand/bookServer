8b98c43dabb0eca40df12d02aab091ae14ab9557
nmguinand <nmguinand@hotmail.com>
2026-01-31 23:17:49 -0500

    included update book route

--- app/api/books/router.py
+++ app/api/books/router.py
@@ -4,7 +4,8 @@ from app.api.books.external_api.books_by_name import router as books_by_name
 from app.api.books.external_api.books_by_isbn import router as books_by_isbn
 from app.api.books.external_api.books_by_generic import router as books_by_generic
 from app.api.books.internal_api.create_book import router as create_book
-from app.api.books.internal_api.get_book import router as books_by_title
+from app.api.books.internal_api.get_book import router as get_books
+from app.api.books.internal_api.update_book import router as update_book
 
 router = APIRouter(prefix="/books", tags=["books"])
 
@@ -12,4 +13,5 @@ router.include_router(books_by_name)
 router.include_router(books_by_isbn)
 router.include_router(books_by_generic)
 router.include_router(create_book)
-router.include_router(books_by_title)
+router.include_router(get_books)
+router.include_router(update_book)
