680a4d13bd20a156f3b90893d9820c198be612d0
nmguinand <nmguinand@hotmail.com>
2026-01-31 21:59:07 -0500

    router has routes from the newly created gets

--- app/api/books/router.py
+++ app/api/books/router.py
@@ -4,6 +4,7 @@ from app.api.books.external_api.books_by_name import router as books_by_name
 from app.api.books.external_api.books_by_isbn import router as books_by_isbn
 from app.api.books.external_api.books_by_generic import router as books_by_generic
 from app.api.books.internal_api.create_book import router as create_book
+from app.api.books.internal_api.get_book import router as books_by_title
 
 router = APIRouter(prefix="/books", tags=["books"])
 
@@ -11,3 +12,4 @@ router.include_router(books_by_name)
 router.include_router(books_by_isbn)
 router.include_router(books_by_generic)
 router.include_router(create_book)
+router.include_router(books_by_title)
