8d73d47c0acda2a64d67be082b5bd2b35ad9bab8
nmguinand <nmguinand@hotmail.com>
2026-02-08 19:43:50 -0500

    utilize commit or raise function for book crud

--- app/crud/book_crud.py
+++ app/crud/book_crud.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from app.crud.author_crud import create_author, get_author_by_name
 from app.crud.genre_crud import get_genre_by_name
 from app.crud.shared_queries import get_book_by_book_id
+from app.db.db_conn import db_manager
 from app.db.db_models import Book
 from app.db.db_models.author import Author
 from app.db.db_models.book_access import BookAccess
@@ -108,7 +109,7 @@ def store_book_entry(book_model: BookModel, session: Session) -> Book:
     book_data.genres = genres
 
     session.add(book_data)
-    session.commit()
+    db_manager.commit_or_raise(session)
     session.refresh(book_data)
 
     return book_data
@@ -228,8 +229,7 @@ def update_book_by_model(book_replacement: BookModel, session: Session) -> None 
                 if sale_info_model.retail_price
                 else None,
             )
-
-    session.commit()
+    db_manager.commit_or_raise(session)
     session.refresh(book_record)
     return book_record
 
@@ -241,5 +241,5 @@ def delete_book_by_book_id(book_id: int, session: Session) -> bool:
         return False
 
     session.delete(book)
-    session.commit()
+    db_manager.commit_or_raise(session)
     return True
