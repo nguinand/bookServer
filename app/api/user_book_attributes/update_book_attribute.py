ce371fc5998c6d638d69318b773bbd8d664b6f6b
nmguinand <nmguinand@hotmail.com>
2026-03-01 23:43:52 -0500

    small refactor on user_book_attributes route

--- app/api/user_book_attributes/update_book_attribute.py
+++ app/api/user_book_attributes/update_book_attribute.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
 from starlette.responses import JSONResponse
 
+from app.crud.user_book_attributes_crud import update_user_book_attribute_entry
 from app.db.db_conn import db_manager
 from app.models.user_book_attributes import UserBookAttributesModel
 from app.utils.logger import get_logger
@@ -20,11 +21,24 @@ async def update_book_attribute(
     book_replacement: UserBookAttributesModel,
     session: Session = Depends(db_manager.get_db),
 ) -> JSONResponse:
-    updated_book_attribute = update_book_attribute(book_replacement, session)
+    try:
+        updated_book_attribute = update_user_book_attribute_entry(
+            book_replacement, session
+        )
+    except ValueError as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
+        )
     if updated_book_attribute:
-        status_code = status.HTTP_200_OK
-        content = {f"Updated book attribute for book id {book_replacement.id}"}
-    else:
-        status_code = status.HTTP_204_NO_CONTENT
-        content = {f"Book attribute not updated for book id {book_replacement.id}"}
-    return JSONResponse(status_code=status_code, content=content)
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "detail": f"Updated book attribute for book id {book_replacement.id}"
+            },
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book attribute not fround.",
+    )
