48eb0e4dccfa88da31c7abe42a3e8c95949d5f6d
nmguinand <nmguinand@hotmail.com>
2026-03-04 23:00:45 -0500

    added create and delete routes

--- app/api/users/router.py
+++ app/api/users/router.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from app.api.users.get_user import router as get_user
+from app.api.users.delete_user import router as delete_user
+from app.api.users.create_user import router as create_user
 
 router = APIRouter()
 
 router.include_router(get_user)
+router.include_router(delete_user)
+router.include_router(create_user)
