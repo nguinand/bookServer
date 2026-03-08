1a7b59e3eaa2a588ca36956d1425db9870a982cc
nmguinand <nmguinand@hotmail.com>
2026-03-08 00:53:33 -0500

    update pre-commit

--- app/api/users/router.py
+++ app/api/users/router.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from app.api.users.get_user import router as get_user
 from app.api.users.delete_user import router as delete_user
 from app.api.users.create_user import router as create_user
+from app.api.users.authenticate_user import router as authenticate_user
 
 router = APIRouter()
 
 router.include_router(get_user)
 router.include_router(delete_user)
 router.include_router(create_user)
+router.include_router(authenticate_user)
