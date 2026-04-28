d71259b5130c3048b057d5b0821ca2fe81922b9e
nmguinand <nmguinand@hotmail.com>
2026-04-27 21:25:24 -0400

    included user update router

--- app/api/users/router.py
+++ app/api/users/router.py
@@ -4,6 +4,7 @@ from app.api.users.delete_user import router as delete_user
 from app.api.users.create_user import router as create_user
 from app.api.users.authenticate_user import router as authenticate_user
 from app.api.users.update_password import router as update_password
+from app.api.users.update_user import router as update_user
 
 router = APIRouter()
 
@@ -12,3 +13,4 @@ router.include_router(delete_user)
 router.include_router(create_user)
 router.include_router(authenticate_user)
 router.include_router(update_password)
+router.include_router(update_user)
